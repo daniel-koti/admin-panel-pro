e42d751358b888c2f082444b928457fb7c12fa05
Daniel Moniz <daniel.moniz@tegra.com.br>
2024-07-08 00:58:37 +0100

    create composition patter for input

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -5,27 +5,27 @@ import {
   Home,
   SquareStack,
   Users,
-  Search,
   LifeBuoy,
   Cog,
+  Search,
 } from 'lucide-react'
 import { Logo } from './logo'
 import { NavItem } from './nav-item'
 import { UsedSpacedWidget } from './used-spaced-widget'
 import { Profile } from './profile'
+import * as Input from '../input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
