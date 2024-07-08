7469e431d400968343c3524edbb1e89882d81dd0
Daniel Moniz <daniel.moniz@tegra.com.br>
2024-07-08 01:27:41 +0100

    create tabs with framer motion and radix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingTabs } from '@/components/settings-tabs'
+
 export default function Home() {
-  return <h1>Hello tailwindcss</h1>
+  return (
+    <section>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingTabs />
+    </section>
+  )
 }
---
+++ src/components/settings-tabs/tab-item.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
+
+export interface TabItemProps {
+  value: string
+  title: string
+  isSelected?: boolean
+}
+
+export function TabItem({ value, title, isSelected = false }: TabItemProps) {
+  return (
+    <Tabs.Trigger
+      value={value}
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
+    >
+      <span>{title}</span>
+
+      {isSelected && (
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
+      )}
+    </Tabs.Trigger>
+  )
+}
